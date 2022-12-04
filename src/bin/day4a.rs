1efaa1fddac6908100d8efb454380b810ebadd39
Felipe Rossi <feliperossi2002@gmail.com>
2022-12-04 14:22:28 +0000

    [Day 4] Part 1 done

---
+++ src/bin/day4a.rs
@@ -0,0 +1,16 @@
+use aoc22::line_reader::LineReader;
+
+fn main() {
+    let mut lr = LineReader::new();
+    let mut answer = 0;
+
+    while lr.read_next().unwrap() > 0 {
+        let numbers = lr.as_numbers();
+        if (numbers[0] >= numbers[2] && numbers[1] <= numbers[3]) || (numbers[2] >= numbers[0] && numbers[3] <= numbers[1]) {
+            answer += 1;
+        }
+    }
+
+    println!("Answer: {}", answer)
+}
+
