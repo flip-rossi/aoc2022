df069e6ab00d1d566600307dbe95fe5f51b35a4e
Felipe Rossi <feliperossi2002@gmail.com>
2022-12-07 14:55:17 +0000

    [Day 7](P1) Removed unnecessary structs

--- src/bin/day7a.rs
+++ src/bin/day7a.rs
@@ -4,43 +4,11 @@ use aoc22::line_reader::LineReader;
 
 const MAX_DIR_SIZE: isize = 100000;
 
-struct Directory {
-    name: String,//Box<String>,
-    files: Vec<File>,
-    sub_dirs: Vec<Directory>,
-    total_size: isize,
-}
-impl Directory {
-    fn new<S: Into<String>>(name: S) -> Self {
-        Self {
-            name: name.into(),
-            files: Vec::new(),
-            sub_dirs: Vec::new(),
-            total_size: 0,
-        }
-    }
-
-    fn add_file(&mut self, file: File) {
-        self.total_size += file.size;
-        self.files.push(file);
-    }
-}
-
-struct File {
-    name: String,
-    size: isize,
-}
-impl File{
-    fn new<S: Into<String>>(name: S, size: isize) -> Self {
-        Self { name: name.into(), size, }
-    }
-}
-
 fn main() {
     let mut lr = LineReader::new();
     let mut answer = 0;
 
-    let mut wd: Vec<Directory> = Vec::new();
+    let mut wd: Vec<isize> = Vec::new();
     while lr.read_next().unwrap() > 0 {
         let mut words = lr.as_words().into_iter();
 
@@ -53,17 +21,17 @@ fn main() {
                             ".." => {
                                 let last_wd = wd.pop().unwrap();
                                 if let Some(dir) = wd.last_mut() {
-                                    dir.add_file(File::new(last_wd.name, last_wd.total_size));
+                                    *dir += last_wd;
                                 }
-                                Some(last_wd.total_size)
+                                Some(last_wd)
                             },
                             "/" => {
                                 wd = Vec::new();
-                                wd.push(Directory::new("/"));
+                                wd.push(0);
                                 None
                             },
-                            dir_name => {
-                                wd.push(Directory::new(dir_name));
+                            _ => {
+                                wd.push(0);
                                 None
                             }
                         }
@@ -82,7 +50,7 @@ fn main() {
                                     match isize::from_str_radix(size_str, 10) {
                                         Ok(size) => {
                                             match words.next() {
-                                                Some(file_name) => wd.last_mut().unwrap().add_file(File::new(file_name, size)),
+                                                Some(_) => *wd.last_mut().unwrap() += size,
                                                 None => panic!("ls: expected file name")
                                             }
                                         }
