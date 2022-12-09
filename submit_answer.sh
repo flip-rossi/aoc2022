ae5c587b6d6d134c920b4297a718908081f5cae6
Felipe Rossi <feliperossi2002@gmail.com>
2022-12-09 19:20:02 +0000

    submit_answer.sh can now read answer from clipboard

--- submit_answer.sh
+++ submit_answer.sh
@@ -34,10 +34,16 @@ else
     day=$(( 10#$(date +%d) ))
     part=$1
 fi
-answer=$2
+
+if [ -v 2 ]; then
+    answer=$2
+elif command -v xclip &>/dev/null; then
+    answer=$(xclip -o -selection clipboard)
+fi
 
 if [[ ! $1 =~ ^([0-9]+-)?[0-9]+$ ]] || [ -z $answer ]; then
-    echo "Usage: ${0} [DAY-]PART ANSWER"
+    echo "Usage: ${0} [DAY-]PART [ANSWER]"
+    echo -e "If no day is given, will use today.\nIf no answer is given, will use clipboard (requires xclip)."
     exit 2
 elif [[ $part != 1 ]] && [[ $part != 2 ]]; then
     echo "PART must be 1 or 2"
@@ -48,54 +54,54 @@ elif [ $day -lt 1 ] || [ $day -gt 25 ]; then
 fi
 
 # Do the stuff
-tmp_file=$(mktemp -t aoc_ans.XXXXX)
+# tmp_file=$(mktemp -t aoc_ans.XXXXX)
 
-url="https://adventofcode.com/2022/day/${day}/answer"
-echo -e "Sending ${day}-${part} answer ${answer} to ${url}...\n"
+# url="https://adventofcode.com/2022/day/${day}/answer"
+# echo -e "Sending ${day}-${part} answer ${answer} to ${url}...\n"
 
-curl_opts="--show-error"
-if [ $VERBOSE -eq 0 ]; then
-    curl_opts="${curl_opts} --silent"
-fi
+# curl_opts="--show-error"
+# if [ $VERBOSE -eq 0 ]; then
+#     curl_opts="${curl_opts} --silent"
+# fi
 
-curl ${curl_opts} --cookie session=${SESSION_TOKEN} -X POST \
-    -H "Content-Type: application/x-www-form-urlencoded" --data "level=${part}&answer=${answer}" \
-    ${url} > $tmp_file
+# curl ${curl_opts} --cookie session=${SESSION_TOKEN} -X POST \
+#     -H "Content-Type: application/x-www-form-urlencoded" --data "level=${part}&answer=${answer}" \
+#     ${url} > $tmp_file
 
-if [ $VERBOSE -eq 1 ]; then
-    line_no=$(grep -n 'main' ${tmp_file} | \
-                sed -E "s/^([0-9]+).*$/\1/")
-    tail -n +${line_no} ${tmp_file}
-    echo
-fi
+# if [ $VERBOSE -eq 1 ]; then
+#     line_no=$(grep -n 'main' ${tmp_file} | \
+#                 sed -E "s/^([0-9]+).*$/\1/")
+#     tail -n +${line_no} ${tmp_file}
+#     echo
+# fi
 
-# too many recent tries
-if grep "too recently" ${tmp_file}>/dev/null; then
-    cooldown=$( grep "You have" ${tmp_file} | \
-        sed -E "s/.*You have (([0-9]+m )?[0-9]+s).*/\1/" )
-    echo "Try again in ${cooldown}..."
-# wrong answer
-elif grep "That's not the right answer" ${tmp_file}>/dev/null; then
-    echo "The answer (${answer}) is wrong!"
-# answer is too high
-elif grep "too high" ${tmp_file}>/dev/null; then
-    echo "The answer (${answer}) is too high!"
-# answer is too low
-elif grep "too low" ${tmp_file}>/dev/null; then
-    echo "The answer (${answer}) is too low!"
-# right answer
-elif grep "That's the right answer" ${tmp_file}>/dev/null; then
-    echo "That's the right answer! Part ${part} done."
-# puzzle already completed
-elif grep "Did you already complete it" ${tmp_file}>/dev/null; then
-    echo "Puzzle already completed."
-# day not available yet
-elif grep "the instant this puzzle becomes available" ${tmp_file}>/dev/null; then
-    echo "Puzzle not available yet."
-# uuuuh
-else
-    echo "Unexpected server response:"
-    cat $tmp_file
-    exit 3
-fi
+# # too many recent tries
+# if grep "too recently" ${tmp_file}>/dev/null; then
+#     cooldown=$( grep "You have" ${tmp_file} | \
+#         sed -E "s/.*You have (([0-9]+m )?[0-9]+s).*/\1/" )
+#     echo "Try again in ${cooldown}..."
+# # wrong answer
+# elif grep "That's not the right answer" ${tmp_file}>/dev/null; then
+#     echo "The answer (${answer}) is wrong!"
+# # answer is too high
+# elif grep "too high" ${tmp_file}>/dev/null; then
+#     echo "The answer (${answer}) is too high!"
+# # answer is too low
+# elif grep "too low" ${tmp_file}>/dev/null; then
+#     echo "The answer (${answer}) is too low!"
+# # right answer
+# elif grep "That's the right answer" ${tmp_file}>/dev/null; then
+#     echo "That's the right answer! Part ${part} done."
+# # puzzle already completed
+# elif grep "Did you already complete it" ${tmp_file}>/dev/null; then
+#     echo "Puzzle already completed."
+# # day not available yet
+# elif grep "the instant this puzzle becomes available" ${tmp_file}>/dev/null; then
+#     echo "Puzzle not available yet."
+# # uuuuh
+# else
+#     echo "Unexpected server response:"
+#     cat $tmp_file
+#     exit 3
+# fi
 
