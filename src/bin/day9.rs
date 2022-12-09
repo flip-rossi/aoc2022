b38eeaac2bf197ea5984c7cecdb388e1b2d02587
Felipe Rossi <feliperossi2002@gmail.com>
2022-12-09 18:24:17 +0000

    [Day 9] Part 2 done

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -4,6 +4,11 @@ use std::collections::HashSet;
 
 use aoc22::solve_puzzle;
 
+const LEFT:  (i32,i32) = (-1, 0);
+const RIGHT: (i32,i32) = ( 1, 0);
+const UP:    (i32,i32) = ( 0, 1);
+const DOWN:  (i32,i32) = ( 0,-1);
+
 struct Motion {
     dir: (i32, i32),
     amount: i32,
@@ -14,14 +19,8 @@ impl Motion {
     }
 }
 
-// const LEFT: &str = "L";
-const LEFT:  (i32,i32) = (-1, 0);
-const RIGHT: (i32,i32) = ( 1, 0);
-const UP:    (i32,i32) = ( 0, 1);
-const DOWN:  (i32,i32) = ( 0,-1);
-
 fn main() {
-    // Parse arguments
+    // Parse input
     let input = std::io::stdin().lines();
     let motions: Vec<Motion> = input.map(|s| {
         let line = s.unwrap();
@@ -62,7 +61,26 @@ fn part1(motions: Vec<Motion>) -> usize {
 }
 
 //=============== PART 2 ===============//
+const KNOTS_N: usize = 10;
+
 fn part2(motions: Vec<Motion>) -> usize {
-    todo!()
+    let mut knots = [(0,0); KNOTS_N];
+
+    let mut tail_positions = HashSet::new();
+    tail_positions.insert(knots[KNOTS_N-1]);
+    for m in motions {
+        for _ in 0..m.amount {
+            knots[0].0 += m.dir.0; knots[0].1 += m.dir.1;
+            for i in 1..knots.len() {
+                let dist = (knots[i-1].0 - knots[i].0, knots[i-1].1 - knots[i].1);
+                if dist.0.abs() >= 2 || dist.1.abs() >= 2 {
+                    knots[i].0 += dist.0.signum(); knots[i].1 += dist.1.signum();
+                }
+            }
+            tail_positions.insert(knots[KNOTS_N-1]);
+        }
+    }
+
+    tail_positions.len()
 }
 
