2b218f2be56f16d9be59d9ff09b441b65035ef65
Felipe Rossi <feliperossi2002@gmail.com>
2022-12-17 03:17:01 +0000

    [Day 15](P2) Tried with a new strategy

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,6 +1,6 @@
 //! Day 15: Beacon Exclusion Zone
 
-use std::collections::{HashSet, HashMap};
+use std::collections::HashSet;
 
 use aoc22::{ solve_puzzle, position::Pos };
 
@@ -28,7 +28,7 @@ fn main() {
 }
 
 //=============== PART 1 ===============//
-fn part1(signal_beacon_pairs: Vec<(Pos,Pos)>) -> i32 {
+fn part1(signal_beacon_pairs: Vec<(Pos,Pos)>) -> i64 {
     let row_wanted = match std::env::args().nth(2) {
         Some(s) => s.parse().expect("Row wanted should be an integer"),
         None => 2_000_000
@@ -38,7 +38,7 @@ fn part1(signal_beacon_pairs: Vec<(Pos,Pos)>) -> i32 {
 
     for (signal, beacon) in signal_beacon_pairs {
         let dist = signal.manh_dist(&beacon);
-        if !(signal.y - dist .. signal.y + dist).contains(&row_wanted) {
+        if row_wanted < signal.y - dist || row_wanted > signal.y - dist {
             continue
         }
 
@@ -51,59 +51,109 @@ fn part1(signal_beacon_pairs: Vec<(Pos,Pos)>) -> i32 {
         }
     }
 
-    excluded.len() as i32
+    excluded.len() as i64
 }
 
 //=============== PART 2 ===============//
 // TODO
-fn part2(signal_beacon_pairs: Vec<(Pos,Pos)>) -> i32 {
+fn part2(signal_beacon_pairs: Vec<(Pos,Pos)>) -> i64 {
     let coords_lim = match std::env::args().nth(2) {
         Some(arg) => arg.parse().expect("Coordenates limit should be an integer"),
         None => 4_000_000
     };
-    let coords_lim_range = 0..=coords_lim;
 
-    //                HashMap<row, HashSet<col>>
-    let mut possible: HashMap<i32, HashSet<i32>> = coords_lim_range.clone()
-        .map(|row| (
-            row,
-            coords_lim_range.clone().collect::<HashSet<i32>>()
-        ))
-        .collect();
+    // let possible_template = coords_lim_range.clone().collect::<HashSet<i32>>();
 
-    for (signal, beacon) in signal_beacon_pairs {
-        let dist = signal.manh_dist(&beacon);
-        for r_row in -dist..=dist {
-            let g_row = signal.y + r_row;
-            if !possible.contains_key(&g_row) {
+    for row in 0..=coords_lim {
+        // eprintln!("ROW {row}");
+        let mut blocked_ranges: Vec<(i32,i32)> = Vec::new();
+        for (signal, beacon) in &signal_beacon_pairs {
+            let dist = signal.manh_dist(&beacon);
+
+            let col_range = dist - (signal.y - row).abs();
+            if col_range < 0 {
                 continue
             }
+            let signal_range = (
+                (signal.x - col_range).clamp(0, coords_lim),
+                (signal.x + col_range).clamp(0, coords_lim)
+            );
 
-            let row = match possible.get_mut(&g_row) {
-                Some(set) => set,
-                None => continue
-            };
+            // eprintln!("{signal_range:?}");
 
-            let col_range = dist - r_row.abs();
-            for r_col in -col_range..=col_range {
-                let g_col = signal.x + r_col;
-                row.remove(&g_col);
+            let mut not_merged = true;
+            let mut i = 0;
+            while i < blocked_ranges.len() {
+                if signal_range.0-1 <= blocked_ranges[i].1 && blocked_ranges[i].0-1 <= signal_range.1 {
+                    // merge_ranges(&mut blocked_ranges, signal_range);
+                    let mut new_range = (
+                        signal_range.0.min(blocked_ranges[i].0),
+                        signal_range.1.max(blocked_ranges[i].1)
+                    );
+                    while let Some(next_r) = blocked_ranges.get(i+1) {
+                        if next_r.0-1 <= new_range.1 {
+                            new_range.0 = new_range.0.min(next_r.0);
+                            new_range.1 = new_range.1.max(next_r.1);
+                            blocked_ranges.remove(i+1);
+                            // eprintln!("merged next");
+                        }
+                        else {
+                            break
+                        }
+                    }
+                    // let mut excluded = 0;
+                    // // if i != 0 {
+                    // //     if let Some(prev_r) = blocked_ranges.get(i-1) {
+                    // //         if new_range.0-1 <= prev_r.1 {
+                    // //             new_range.0 = new_range.0.min(prev_r.0);
+                    // //             new_range.1 = new_range.1.max(prev_r.1);
+                    // //             blocked_ranges.remove(i-1);
+                    // //             excluded = 1;
+                    // //             eprintln!("merged prev");
+                    // //         }
+                    // //     }
+                    // // }
+                    // eprintln!("new range {new_range:?}");
+                    blocked_ranges[i] = new_range;
+                    not_merged = false;
+                    break
+                }
+                else if signal_range.1 < blocked_ranges[i].0-1 {
+                    blocked_ranges.insert(i, signal_range);
+                    break
+                }
+                i += 1;
             }
-
-            if row.is_empty() {
-                possible.remove(&g_row);
+            if not_merged {
+                blocked_ranges.push(signal_range);
             }
+            // eprintln!("{blocked_ranges:?}");
         }
+        // eprintln!("ROW {row}: {blocked_ranges:?}");
+        if blocked_ranges.len() >= 2 { // || blocked_ranges[0].0 > 0 || blocked_ranges[blocked_ranges.len()-1].1 < coords_lim {
+            eprintln!("{blocked_ranges:?} on x={}, y={row}", blocked_ranges[0].1+1);
+            return (blocked_ranges[0].1+1) as i64 * 4_000_000 + row as i64
+        }
+        
     }
 
-    let beacon_pos = match possible.iter().next() {
-        Some((row, set)) => match set.iter().next() {
-            Some(col) => Pos::new(*col, *row),
-            None => panic!("Set was empty at row {row}")
-        }
-        None => panic!("Map was empty")
-    };
+    panic!("empty space not found")
+}
 
-    beacon_pos.x * 4_000_000 + beacon_pos.y
+fn merge_ranges(ranges: &mut Vec<(i32,i32)>, mut signal_range: (i32,i32)) {
+    let mut to_remove = Vec::with_capacity(ranges.len());
+    for (i, r) in ranges.iter().enumerate() {
+        if signal_range.0-1 <= r.1 && r.0-1 <= signal_range.1 {
+            to_remove.push(i);
+            signal_range = (
+                signal_range.0.min(r.0),
+                signal_range.1.max(r.1)
+            );
+        }
+    }
+    for &i in to_remove.iter().rev() {
+        ranges.remove(i);
+    }
+    ranges.insert(to_remove[0], signal_range);
 }
 
