406bfd14ed7a01d6909660e6c925b725f95ddc30
Felipe Rossi <feliperossi2002@gmail.com>
2022-12-21 12:10:35 +0000

    Changed solution template

--- template.rs
+++ template.rs
@@ -1,18 +1,26 @@
 //! Day #: {Title}
 
-use aoc22::solve_puzzle;
-
 fn main() {
-    let answer = solve_puzzle!();
+    // Parse input
+    /*
+    for line in std::io::stdin().lines() {
+        // ...
+    }
+    */
+
+    // Solve
+    // let answer = aoc22::solve_puzzle!();
     // println!("{answer}")
 }
 
 //=============== PART 1 ===============//
+#[allow(dead_code, unused_variables)]
 fn part1() -> ! {
     todo!()
 }
 
 //=============== PART 2 ===============//
+#[allow(dead_code, unused_variables)]
 fn part2() -> ! {
     todo!()
 }
